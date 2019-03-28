610516a16241f561400728a8fd3232236d50b9c3
Tobias Scholze <tobiasscholze@tobiass-mbp.lan>
2019-03-28 20:56:32 +0100

    Added first call to send and reponsed to POST calls

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        return render_template("index.html", results="Bla")
+    else:
+        return render_template("index.html")
 
 
-@app.route("/meep")
-def meep():
-    return render_template("meep.html")
+def get_team_members():
+    return ""
+
 
 if __name__ == "__main__":
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
