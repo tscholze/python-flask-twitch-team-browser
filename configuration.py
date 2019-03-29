9dcf3b995791bc4ce9d2053cfe55a179fbe97e68
Tobias Scholze <tobiasscholze@tobiass-mbp.lan>
2019-03-29 18:52:18 +0100

    Added first prototype of data requesting and table rendering in my very first Twitch Stream

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# Twitch API client id (https://dev.twitch.tv/console/apps)
+TWITCH_CLIENT_ID = "<KEY>"
+
+# Computed API teams endpoint url
+TWITCH_TEAMS_BASE_ENDPOINT_URL = "https://api.twitch.tv/kraken/teams/"
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request
+import configuration
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    # If request is Post, request data from server.
     if request.method == "POST":
-        return render_template("index.html", results="Bla")
+        # Get information from POST request.
+        team_name = request.form.get("team_name")
+        include_mature = True if request.form.get("include_mature") else False
+
+        # Team name has to be set, otherwise return default page.
+        if team_name is None:
+            # TODO: Return error string to inform the user
+            return "Error"
+
+        # Return template with requested results.
+        return render_template("index.html", results=get_team_members(team_name, include_mature))
     else:
         return render_template("index.html")
 
+def get_team_members(team_name, include_mature):
+    headers = {
+        'Accept': 'application/vnd.twitchtv.v5+json',
+        'Client-ID': configuration.TWITCH_CLIENT_ID 
+    }
+    url = configuration.TWITCH_TEAMS_BASE_ENDPOINT_URL + "/" + team_name
+    members = requests.get(url, headers=headers).json()["users"]
 
-def get_team_members():
-    return ""
+    if include_mature == False:
+        members = filter(lambda member: member["mature"] == False, members)
+
+    return members
 
 
 if __name__ == "__main__":
