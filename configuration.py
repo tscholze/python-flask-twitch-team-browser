a655c0aa549add84b89942999bb47a9a424091f2
Tobias Scholze <tobiasscholze@tobiass-mbp.lan>
2019-03-31 15:25:08 +0200

    Added alternating row color to member table

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Twitch API client id (https://dev.twitch.tv/console/apps)
-TWITCH_CLIENT_ID = "0z8qotczomybi8kl93f84bf9ju0nqy"
+TWITCH_CLIENT_ID = "<KEY>"
 
 # Computed API teams endpoint url
 TWITCH_TEAMS_BASE_ENDPOINT_URL = "https://api.twitch.tv/kraken/teams/"
